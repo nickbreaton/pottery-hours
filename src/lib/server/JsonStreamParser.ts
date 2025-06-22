f88d003c4320ee8378b0fed097f1a47a882214d9
Nick Breaton <nick@nickbreaton.com>
2025-06-21 20:26:34 -0400

    clean up

---
+++ src/lib/server/JsonStreamParser.ts
@@ -0,0 +1,57 @@
+import { Channel, Schema, Sink, Chunk, Data, Effect } from 'effect';
+import oboe from 'oboe';
+
+class JsonStreamParseError extends Data.TaggedError('JsonStreamError')<{
+	cause: unknown;
+}> {}
+
+export class JsonStreamParser {
+	static makeSink<A, I, R>(schema: Schema.Schema<A, I, R>) {
+		const ob = oboe();
+		const nil = Symbol('nil');
+
+		let parsed: unknown = nil;
+		let error: oboe.FailReason | typeof nil = nil;
+
+		ob.node('![*]', (item: unknown) => {
+			parsed = item;
+		});
+
+		ob.fail((cause) => {
+			error = cause;
+		});
+
+		const channel = Channel.readWith({
+			onInput: (chunk: Chunk.Chunk<unknown>) => {
+				Chunk.forEach(chunk, (c) => {
+					if (typeof c === 'string') {
+						ob.emit('data', c);
+					}
+				});
+
+				if (parsed !== nil) {
+					let actual = parsed;
+					parsed = nil;
+					return Channel.succeed(Chunk.of(actual));
+				}
+
+				if (error !== nil) {
+					return Channel.fail(new JsonStreamParseError({ cause: error }));
+				}
+
+				return Channel.succeed(Chunk.empty<unknown>());
+			},
+			onFailure: (error) => Channel.fail(new JsonStreamParseError({ cause: error })),
+			onDone: () => Channel.succeed(Chunk.empty<unknown>())
+		});
+
+		return Sink.fromChannel(channel).pipe(
+			Sink.mapEffect((chunk) => {
+				return Effect.gen(function* () {
+					const effects = Chunk.map(chunk, (part) => Schema.decodeUnknown(schema)(part));
+					return Chunk.fromIterable(yield* Effect.all(effects));
+				});
+			})
+		);
+	}
+}
--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,15 +1,12 @@
 import { runtime } from '$lib/server/runtime';
-import { Channel, Console, Effect, Schema, Sink, Stream, Chunk, Data } from 'effect';
+import { Console, Effect, Stream } from 'effect';
 import { AnthropicClient } from '$lib/server/AnthropicClient';
 import { ChatEntry } from '$lib/server/schema';
 import type { RequestHandler } from './$types';
-import oboe from 'oboe';
+import { JsonStreamParser } from '$lib/server/JsonStreamParser';
+import { HttpServerResponse } from '@effect/platform';
 
-class JsonStreamError extends Data.TaggedError('JsonStreamError')<{
-	cause: unknown;
-}> {}
-
-export const POST: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async ({ request, url }) => {
 	const program = Effect.gen(function* () {
 		const anthropic = yield* AnthropicClient;
 
@@ -20,64 +17,20 @@ export const POST: RequestHandler = async ({ request }) => {
 			throw new Error('Expected a file to be uploaded');
 		}
 
-		// // Acquire the resource once for the entire stream
-		// const resource = yield* Effect.acquireRelease(
-		// 	Effect.sync(() => {
-		// 		const resource = Math.random();
-		// 		console.log('Acquired resource:', resource);
-		// 		return resource;
-		// 	}),
-		// 	(resource) => Effect.sync(() => console.log('Released resource:', resource))
-		// );
-
-		const ob = oboe();
-		const nil = Symbol('nil');
-
-		let parsed: unknown = nil;
-		let error: oboe.FailReason | typeof nil = nil;
-
-		// TODO: This is not async, but what if it was? How could we make it async?
-		ob.node('![*]', (item: unknown) => {
-			parsed = item;
-		});
-
-		ob.fail((cause) => {
-			error = cause;
-		});
-
-		const textStream = anthropic.send(file).pipe(
-			Stream.unwrap,
-			Stream.transduce(
-				Sink.fromChannel(
-					Channel.readWith({
-						onInput: (chunk: Chunk.Chunk<string>) => {
-							Chunk.forEach(chunk, (c) => ob.emit('data', c));
-
-							if (parsed !== nil) {
-								let actual = parsed;
-								parsed = nil;
-								return Channel.succeed(Chunk.of(actual));
-							}
-
-							if (error !== nil) {
-								return Channel.fail(Chunk.of(new JsonStreamError({ cause: error })));
-							}
-
-							return Channel.succeed(Chunk.empty<unknown>());
-						},
-						onFailure: (error) => Channel.fail(error),
-						onDone: () => Channel.succeed(Chunk.empty<unknown>())
-					})
-				)
-			),
-			Stream.flattenChunks
+		return Stream.unwrap(anthropic.send(file)).pipe(
+			Stream.transduce(JsonStreamParser.makeSink(ChatEntry)),
+			Stream.flattenChunks,
+			Stream.map(JSON.stringify),
+			Stream.tapErrorCause((error) => Effect.logError('Stream failed with error', error)),
+			Stream.toReadableStream
 		);
-
-		// return yield* Stream.runForEach(textStream, (chunk) => Console.log(chunk));
-		return yield* Stream.toReadableStreamEffect(textStream.pipe(Stream.map(JSON.stringify)));
 	});
 
-	const stream = await runtime.runPromise(program.pipe(Effect.scoped, Effect.withSpan('POST')));
+	const readable = await program.pipe(
+		Effect.scoped,
+		Effect.withSpan('POST', { attributes: { pathname: url.pathname } }),
+		runtime.runPromise
+	);
 
-	return new Response(stream);
+	return new Response(readable);
 };
