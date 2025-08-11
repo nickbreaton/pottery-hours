c5070b8e5bf628e0c3ddf1a7d8c36f88c6629489
Nick Breaton <nick@nickbreaton.com>
2025-08-11 16:49:56 -0400

    Delete old

--- src/lib/old/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import { ParseResult, Schema } from 'effect';
-
-export const FileFromSelf = Schema.declare((input: unknown): input is File => {
-	return input instanceof File;
-});
-
-export const PdfFile = FileFromSelf.pipe(Schema.filter((file) => file.type === 'application/pdf'));
-
-export type PdfFile = typeof PdfFile.Type;
