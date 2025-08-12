561f98f49c01665b63fbf4db1456075481ddd236
Nick Breaton <nick@nickbreaton.com>
2025-08-12 16:13:11 -0400

    Whitespace fixes

--- src/lib/server/ScheduleAnalyzer.ts
+++ src/lib/server/ScheduleAnalyzer.ts
@@ -25,27 +25,27 @@ export class ScheduleAnalyzer extends Effect.Service<ScheduleAnalyzer>()('Schedu
 				});
 
 				const system = dedent`
-				  You are an assistant which can parse a pottery studio schedule, outputting that schedule in a JSON format.
+          You are an assistant which can parse a pottery studio schedule, outputting that schedule in a JSON format.
 
-				  A PDF representation of a pottery studio schedule will be provided to you. This schedule designates open studio hours.
-					Its original form is a spreadsheet laid out as a calendar weeks starting Sunday and ending on Saturday.
-					Each day with available hours is represented by a box with a time range.
+          A PDF representation of a pottery studio schedule will be provided to you. This schedule designates open studio hours.
+          Its original form is a spreadsheet laid out as a calendar weeks starting Sunday and ending on Saturday.
+          Each day with available hours is represented by a box with a time range.
 
-					Here are some rules to keep in mind:
+          Here are some rules to keep in mind:
 
-					* There can sometimes be multiple blocks of time per day. THIS IS REALLY REALLY IMPORTANT.
-					* Dates always appear above the box containing the hours.
-					* Only entries for dates that have hours defined should be included in the result.
-					* Each week also has a description in the left column which should be included in the results.
-					* Sometimes a more detailed description is provided in the box itself. Do not include times in the label.
-					* If there is no description, use the title of the sheet as fallback - keeping it as brief as possible, ideally under 4 words.
-					* Use only a singular fallback label. Typically its best to indicate "Open Studio" or similar with the label.
-					* Its rare to have more than two sets of hours defined per day.
-					* Do NOT include any dates that do not have hours defined, this typically means the studio is closed which is not relevant.
-					* DO NOT MAKE UP DATA WHICH DOES NOT APPEAR IN THE PDF.
+          * There can sometimes be multiple blocks of time per day. THIS IS REALLY REALLY IMPORTANT.
+          * Dates always appear above the box containing the hours.
+          * Only entries for dates that have hours defined should be included in the result.
+          * Each week also has a description in the left column which should be included in the results.
+          * Sometimes a more detailed description is provided in the box itself. Do not include times in the label.
+          * If there is no description, use the title of the sheet as fallback - keeping it as brief as possible, ideally under 4 words.
+          * Use only a singular fallback label. Typically its best to indicate "Open Studio" or similar with the label.
+          * Its rare to have more than two sets of hours defined per day.
+          * Do NOT include any dates that do not have hours defined, this typically means the studio is closed which is not relevant.
+          * DO NOT MAKE UP DATA WHICH DOES NOT APPEAR IN THE PDF.
 
-					Parse the provided document and return its result in a JSON format for the hours for each day, without any additional output.
-					Here is an example of the JSON format. Remember include absolutely no additional text or information. DO NOT FORMAT LIKE MARKDOWN.
+          Parse the provided document and return its result in a JSON format for the hours for each day, without any additional output.
+          Here is an example of the JSON format. Remember include absolutely no additional text or information. DO NOT FORMAT LIKE MARKDOWN.
 
 					[
 						{ "month": "January", "day": 1, "year": ${currentYear}, "label": "Winter Session", "hours": [ { "start_hour": 9, "start_minute": 0, "start_meridiem": "AM", "end_hour": 2, "end_minute": 0, "end_meridiem": "PM" } ] },
