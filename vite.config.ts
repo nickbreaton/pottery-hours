04db7d042aaa2e1075ac04b4749d8b15826f568c
Nick Breaton <nick@nickbreaton.com>
2025-08-13 14:46:11 -0400

    Ignore platform module for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+console.log(process.env);
+
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+		alias: {
+			'@effect/platform-node': 'noop'
+		}
+	}
 });
