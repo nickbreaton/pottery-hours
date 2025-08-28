34d4e6fcf248c14a01a4bff9ed4bd3d3ed9c8717
Nick Breaton <nick@nickbreaton.com>
2025-08-27 20:22:28 -0400

    More mobile tweaks

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,7 @@
 			<input
 				type="password"
 				name="password"
-				class="border-[1.5px] bg-white text-zinc-900 border-zinc-200/75 placeholder-zinc-400/75 outline-none rounded-lg resize-none p-2 w-sm max-w-lg focus:outline-3 focus:border-purple-200 focus:outline-solid focus:outline-purple-100"
+				class="border-[1.5px] bg-white text-zinc-900 border-zinc-200/75 placeholder-zinc-400/75 outline-none rounded-lg resize-none p-2 w-sm max-w-full focus:outline-3 focus:border-purple-200 focus:outline-solid focus:outline-purple-100"
 			/>
 		</label>
 		<button type="submit" class="sr-only">Login</button>
