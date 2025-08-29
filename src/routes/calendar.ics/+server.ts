72f7f631e4718701207305b7b85d636c7b8ce754
Nick Breaton <nick@nickbreaton.com>
2025-08-28 22:34:34 -0400

    Add logs

--- src/routes/calendar.ics/+server.ts
+++ src/routes/calendar.ics/+server.ts
@@ -3,15 +3,19 @@ import { runtime } from '$lib/server/runtime';
 import { Effect } from 'effect';
 import type { RequestHandler } from '../api/new/$types';
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, request }) => {
 	const handler = Effect.gen(function* () {
 		const repo = yield* CalendarRepo;
+
+		yield* Effect.log(`Request for calendar feed`).pipe(
+			Effect.annotateLogs({ userAgent: request.headers.get('User-Agent') })
+		);
+
 		return yield* repo.feed({ origin: url.origin });
 	});
 
 	const response = await runtime.runPromise(handler);
 
-	// TODO: ics format
 	return new Response(response, {
 		headers: url.searchParams.has('format', 'text')
 			? {}
